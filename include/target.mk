6880f9e91d1b020830af7d132d9b1d655b03f3e3
gavin219 <jiabin.song@foxmail.com>
2018-11-08 01:12:36 +0800

    Update include/target.mk

--- include/target.mk
+++ include/target.mk
@@ -13,11 +13,14 @@ __target_inc=1
 DEVICE_TYPE?=router
 
 # Default packages - the really basic set
-DEFAULT_PACKAGES:=base-files libc libgcc busybox dropbear mtd uci opkg netifd fstools uclient-fetch logd
+DEFAULT_PACKAGES:=base-files libc libgcc busybox dropbear mtd uci opkg netifd fstools uclient-fetch logd \
+kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw \
+default-settings luci luci-app-upnp \
+luci-app-sfe luci-app-flowoffload
 # For nas targets
 DEFAULT_PACKAGES.nas:=block-mount fdisk lsblk mdadm
 # For router targets
-DEFAULT_PACKAGES.router:=dnsmasq iptables ip6tables ppp ppp-mod-pppoe firewall odhcpd-ipv6only odhcp6c kmod-ipt-offload
+DEFAULT_PACKAGES.router:=dnsmasq iptables ip6tables +kmod-ipt-nat6 +wget ppp ppp-mod-pppoe firewall odhcpd-ipv6only odhcp6c kmod-ipt-offload
 DEFAULT_PACKAGES.bootloader:=
 
 ifneq ($(DUMP),)
